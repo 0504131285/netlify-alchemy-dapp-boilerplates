6b4e11d7a9a08bec5993eddff8c1d6504a85169a
Jimmy <143755915+0504131285@users.noreply.github.com>
2024-06-20 16:16:20 -0500

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -30,7 +30,7 @@ export default function InstructionsComponent() {
 				>
 					<div className={styles.button}>
 						{/* <img src="https://static.alchemyapi.io/images/cw3d/Icon%20Medium/lightning-square-contained-m.svg" width={"20px"} height={"20px"} /> */}
-						<p>Explore Templatesssssss</p>
+						<p>Explore Templates Jimmy</p>
 					</div>
 				</a>
 				<a
