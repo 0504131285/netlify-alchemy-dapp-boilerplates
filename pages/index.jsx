9fdb63ee2f1365407934c21fd7b6a0bfa5579315
Jimmy <143755915+0504131285@users.noreply.github.com>
2024-06-20 16:18:43 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Home.module.css";
-import InstructionsComponent from "../components/InstructionsComponent";
+import InstructionsComponent from "../components/InstructionsComponent.jsx";
 
 export default function Home() {
   return (
